2477bd8a93164c02a90feeaa1ead4a2098c6be79
TianxingChen <chentianxing2002@gmail.com>
2026-02-03 18:00:46 +0800

    support Dual_X_Arm_Node

--- hardware/robot/__init__.py
+++ hardware/robot/__init__.py
@@ -1,6 +1,8 @@
 from .base_robot import *
 from .dual_x_arm import Dual_X_Arm
+from .dual_x_arm_node import Dual_X_Arm_Node
 
 ROBOT_REGISTRY = {
-    "dual_x_arm": Dual_X_Arm
+    "dual_x_arm": Dual_X_Arm,
+    "dual_x_arm_node": Dual_X_Arm_Node
 }
