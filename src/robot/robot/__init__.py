541d4bbd27a3d4e2dad49446c0f789d0fcc55488
Tian-Nian <1250576969@qq.com>
2026-02-04 11:46:11 +0800

    add test robot sample.

--- src/robot/robot/__init__.py
+++ src/robot/robot/__init__.py
@@ -1,6 +1,8 @@
 from .base_robot import *
 from .dual_x_arm import Dual_X_Arm
+from .dual_test_robot import Dual_Test_Robot
 
 ROBOT_REGISTRY = {
-    "dual_x_arm": Dual_X_Arm
+    "dual_x_arm": Dual_X_Arm,
+    "dual_test_robot": Dual_Test_Robot,
 }
---
+++ src/robot/robot/dual_test_robot.py
@@ -0,0 +1,52 @@
+from robot.robot.base_robot import Robot
+from robot.controller.TestArm_controller import TestArmController
+from robot.sensor.TestVision_sensor import TestVisonSensor
+from datetime import datetime
+import time
+
+class Dual_Test_Robot(Robot):
+    def __init__(self, config, start_episode=0):
+        super().__init__(config=config, start_episode=start_episode)
+        self.first_start = True
+        self.config = config
+        self.controllers = {
+            "arm":{
+                "left_arm": TestArmController("left_arm"),
+                "right_arm": TestArmController("right_arm"),
+            },
+        }
+        self.sensors = {
+            "image": {
+                "cam_head": TestVisonSensor("cam_head"),
+                "cam_left_wrist": TestVisonSensor("cam_left_wrist"),
+                "cam_right_wrist": TestVisonSensor("cam_right_wrist"),
+            },
+        }
+
+    def set_up(self, teleop=False):
+        super().set_up()
+        self.teleop_mode = teleop
+        self.controllers["arm"]["left_arm"].set_up()
+        self.controllers["arm"]["right_arm"].set_up()
+
+        self.sensors["image"]["cam_head"].set_up(is_depth=False, is_jpeg=True)
+        self.sensors["image"]["cam_left_wrist"].set_up(is_depth=False, is_jpeg=True)
+        self.sensors["image"]["cam_right_wrist"].set_up(is_depth=False, is_jpeg=True)
+        
+        self.set_collect_type({"arm": ["joint", "qpos", "gripper"], "image": ["color"]})
+        print(f"[{datetime.now():%Y-%m-%d %H:%M:%S}] ✅ Setup complete.")
+
+    def reset(self):
+        move_data = {
+            "arm":{
+                "left_arm":{
+                    "joint": self.config['robot']['init_qpos']['left_arm'],
+                    "gripper":  self.config['robot']['init_qpos']['left_gripper'],
+                },
+                "right_arm":{
+                    "joint": self.config['robot']['init_qpos']['right_arm'],
+                    "gripper":  self.config['robot']['init_qpos']['right_gripper'],
+                }
+            }
+        }
+        self.move(move_data)
