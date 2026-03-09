f8554227b728da88c5576e0c701e0fabe371c136
Tian-Nian <1250576969@qq.com>
2026-03-09 16:41:25 +0800

    update

--- tools/pedal_test.py
+++ tools/pedal_test.py
@@ -1,7 +1,9 @@
+import sys
 from robot.utils.base.footpedal import FootPedal
 import time
 
-footpedal = FootPedal("/dev/pedal")
+device_path = sys.argv[1] if len(sys.argv) > 1 else "/dev/pedal"
+footpedal = FootPedal(device_path)
 
 while not footpedal.was_pressed():
     print("等待踏板按下...")
